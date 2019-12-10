fc8b06e52be03060a3223142c723589382fadad4
hyecheon <rainbow880616@gmail.com>
2019-12-10 09:00:33 +0900

    Context refactoring

---
+++ src/api.js
@@ -0,0 +1,14 @@
+import Axios from "axios";
+
+export async function getUsers() {
+  const response = await Axios.get(
+    "https://jsonplaceholder.typicode.com/users/"
+  );
+  return response.data;
+}
+export async function getUser(id) {
+  const response = await Axios.get(
+    `https://jsonplaceholder.typicode.com/users/${id}`
+  );
+  return response.data;
+}
